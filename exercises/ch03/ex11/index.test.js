c26cb3670b8a6bbf66ffa3d8550ede9d859eca6c
skkotaro <skworks68@gmail.com>
2025-04-15 17:38:50 +0900

    テストのタイトルの修正

--- exercises/ch03/ex11/index.test.js
+++ exercises/ch03/ex11/index.test.js
@@ -1,22 +1,22 @@
 import { equals } from "./index.js";
 
 describe("equals", () => {
-  it("Returns comparisons between objects with the same value", () => {
+  it("returns comparisons between objects with the same value", () => {
     expect(equals(42, 42)).toBe(true);
     expect(equals(null, null)).toBe(true);
   });
 
-  it("Returns comparisons between objects of different values", () => {
+  it("returns comparisons between objects of different types", () => {
     expect(equals({x: 42}, 42)).toBe(false);
     expect(equals(null, {x: 42})).toBe(false);
   });
 
-  it("Returns a comparison of obj1 and obj1 objects whose elements are not aligned", () => {
+  it("returns false when comparisons objects with different property structures", () => {
     expect(equals({x: 1}, {y: 1})).toBe(false);
     expect(equals({x: 1}, {x: 1, y: 1})).toBe(false);
   });
 
-  it("Returns a comparison of obj1 and an object with additions to the elements of obj1", () => {
+  it("returns a comparison of object and an object with additions to the elements", () => {
     expect(equals({x: {y: {z: 10}}}, {x: {y: {z: 10}}})).toBe(true);
     expect(equals({x: {y: {z: 10}}}, {x: {y: {z: 10, w: 1}}})).toBe(false);
   });
