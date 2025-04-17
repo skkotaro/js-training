83e923877d815b14bdb4201188a793656792e1b7
skkotaro <skworks68@gmail.com>
2025-04-17 21:41:23 +0900

    ch02/ex05の修正

--- exercises/ch02/ex05/index.js
+++ exercises/ch02/ex05/index.js
@@ -11,16 +11,16 @@
 // value instead of null when the key is not in the map
 class DefaultMap extends Map {
   constructor(defaultValue) {
-    super(); // Invoke superclass constructor
-    this.defaultValue = defaultValue; // Remember the default value
+    super() // Invoke superclass constructor
+    this.defaultValue = defaultValue // Remember the default value
   }
 
   get(key) {
     if (this.has(key)) {
       // If the key is already in the map
-      return super.get(key); // return its value from superclass.
+      return super.get(key) // return its value from superclass.
     } else {
-      return this.defaultValue; // Otherwise return the default value
+      return this.defaultValue // Otherwise return the default value
     }
   }
 }
@@ -28,27 +28,27 @@ class DefaultMap extends Map {
 // This class computes and displays letter frequency histograms
 class Histogram {
   constructor() {
-    this.letterCounts = new DefaultMap(0); // Map from letters to counts
-    this.totalLetters = 0; // How many letters in all
+    this.letterCounts = new DefaultMap(0) // Map from letters to counts
+    this.totalLetters = 0 // How many letters in all
   }
 
   // This function updates the histogram with the letters of text.
   add(text) {
     // Remove whitespace from the text, and convert to upper case
-    text = text.replace(/\s/g, "").toUpperCase();
+    text = text.replace(/\s/g, "").toUpperCase()
 
     // Now loop through the characters of the text
     for (let character of text) {
-      let count = this.letterCounts.get(character); // Get old count
-      this.letterCounts.set(character, count + 1); // Increment it
-      this.totalLetters++;
+      let count = this.letterCounts.get(character) // Get old count
+      this.letterCounts.set(character, count + 1) // Increment it
+      this.totalLetters++
     }
   }
 
   // Convert the histogram to a string that displays an ASCII graphic
   toString() {
     // Convert the Map to an array of [key,value] arrays
-    let entries = [...this.letterCounts];
+    let entries = [...this.letterCounts]
 
     // Sort the array by count, then alphabetically
     entries.sort(
@@ -56,20 +56,20 @@ class Histogram {
       // A function to define sort order.
       if (a[1] === b[1]) {
         // If the counts are the same
-        return a[0] < b[0] ? -1 : 1; // sort alphabetically.
+        return a[0] < b[0] ? -1 : 1 // sort alphabetically.
       } else {
         // If the counts differ
-        return b[1] - a[1]; // sort by largest count.
+        return b[1] - a[1] // sort by largest count.
       }
-    });
+    })
 
     // Convert the counts to percentages
     for (let entry of entries) {
-      entry[1] = (entry[1] / this.totalLetters) * 100;
+      entry[1] = (entry[1] / this.totalLetters) * 100
     }
 
     // Drop any entries less than 1%
-    entries = entries.filter((entry) => entry[1] >= 1);
+    entries = entries.filter((entry) => entry[1] >= 1)
 
     // Now convert each entry to a line of text
     let lines = entries.map(
@@ -77,7 +77,7 @@ class Histogram {
     );
 
     // And return the concatenated lines, separated by newline characters.
-    return lines.join("\n");
+    return lines.join("\n")
   }
 }
 
@@ -85,16 +85,16 @@ class Histogram {
 // asynchronously reads chunks of text from standard input, and adds those chunks to
 // the histogram. When it reaches the end of the stream, it returns this histogram
 async function histogramFromStdin() {
-  process.stdin.setEncoding("utf-8"); // Read Unicode strings, not bytes
-  let histogram = new Histogram();
+  process.stdin.setEncoding("utf-8") // Read Unicode strings, not bytes
+  let histogram = new Histogram()
   for await (let chunk of process.stdin) {
-    histogram.add(chunk);
+    histogram.add(chunk)
   }
-  return histogram;
+  return histogram
 }
 
 // This one final line of code is the main body of the program.
 // It makes a Histogram object from standard input, then prints the histogram.
 histogramFromStdin().then((histogram) => {
-  console.log(histogram.toString());
-});
+  console.log(histogram.toString())
+})
